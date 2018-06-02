8226dc0d57078b73954647582102affcc0fbec79
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-06-02 17:30:43 +0900

    ceparate methods

--- stackTest.php
+++ stackTest.php
@@ -3,16 +3,31 @@ use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase
 {
-    public function testPushAndPop()
+    public function testEmpty()
     {
         $stack = [];
         $this->assertSame(0, count($stack));
-        
+        return $stack;
+    }
+
+    /**
+    * @depends testEmpty
+    **/
+    public function testPush(array $stack)
+    {
         array_push($stack, 'foo');
         $this->assertSame(1, count($stack));
         $this->assertSame('foo', $stack[count($stack)-1]);
+        return $stack;
+    }
 
-        $this->assertSame('foo', array_pop($stack));
+    /**
+    * @depends testPush
+    **/
+    public function testPop(array $stack)
+    {
+         $this->assertSame('foo', array_pop($stack));
         $this->assertSame(0, count($stack));
     }
+
 }
