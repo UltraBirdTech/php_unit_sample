600b8614bdd27713154fc02398ee1e34998e3fa5
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-06-02 16:51:56 +0900

    add pop test

--- stackTest.php
+++ stackTest.php
@@ -7,5 +7,12 @@ class StackTest extends TestCase
     {
         $stack = [];
         $this->assertSame(0, count($stack));
+        
+        array_push($stack, 'foo');
+        $this->assertSame(1, count($stack));
+        $this->assertSame('foo', $stack[count($stack)-1]);
+
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertSame(0, count($stack));
     }
 }
